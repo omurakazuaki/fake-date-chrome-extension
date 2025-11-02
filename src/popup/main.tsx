376c84885999c0617940fa51b4a1f45730fe9472
omurakazuaki <kazomur@gmail.com>
2025-11-02 16:19:45 +0900

    fix: package update

--- src/popup/main.tsx
+++ src/popup/main.tsx
@@ -1,12 +1,14 @@
-import { ThemeProvider } from '@mui/material/styles'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Form from './components/Form.tsx'
 import './main.css'
 
+const theme = createTheme()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={{}}>
+    <ThemeProvider theme={theme}>
       <Form />
     </ThemeProvider>
   </StrictMode>,
